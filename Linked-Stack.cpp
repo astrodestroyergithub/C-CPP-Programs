c50de0737457711b9699b6c4e8b7492ddb7dc411
Tamojit Roy <67942951+astrodestroyergithub@users.noreply.github.com>
2021-07-20 11:27:01 +0530

    Created linked stack

---
+++ Linked-Stack.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+struct stack{
+		int data;
+		struct stack *next;
+	}*top=NULL, *nw;
+	
+void push()
+{
+	nw=(struct stack *)malloc(sizeof(struct stack));
+	if(nw==NULL)
+	{
+		printf("Stack is empty");
+	}
+	else
+	{
+		int x;
+		printf("Enter a value");
+		scanf("%d",&x);
+		nw->data=x;
+		nw->next=top; top=nw;
+	}
+}
+
+void pop()
+{
+	if(top==NULL)
+	{
+		printf("Stack is empty");
+	}
+	else
+	{
+		printf("%d is deleted",top->data);
+		struct stack *temp;
+		temp=top;
+		top=top->next;
+		free(temp);
+	}
+}
+
+int main()
+{
+	push();	
+	return 0;
+}
