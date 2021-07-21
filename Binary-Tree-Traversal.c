276a86907a93223bbe52fc6a65a1f715dc30d2f3
Tamojit Roy <67942951+astrodestroyergithub@users.noreply.github.com>
2021-07-21 12:52:56 +0530

    Beautified

--- Binary-Tree-Traversal.c
+++ Binary-Tree-Traversal.c
@@ -1,45 +1,35 @@
 #include<stdio.h>
 
-struct node
-{
-	int info;
-	struct node *left;
-	struct node *right;
-}*temp, *nw;
+struct node {
+  int info;
+  struct node * left;
+  struct node * right;
+}* temp, * nw;
 
-struct node *makeNode(int x)
-{
-	temp=(struct node*)malloc(sizeof(struct node));
-	temp->info=x;
-	temp->left=NULL;
-	temp->right=NULL;
-	return temp;
+struct node * makeNode(int x) {
+  temp = (struct node * ) malloc(sizeof(struct node));
+  temp -> info = x;
+  temp -> left = NULL;
+  temp -> right = NULL;
+  return temp;
 }
 
-struct node *leftChild(struct node *p, int x)
-{
-	if(p->left==NULL)
-	{
-		p->left=makeNode(x);
-	}
-	else
-	{
-		printf("Left-child already exists\n");
-	}
-	return (p->left);
+struct node * leftChild(struct node * p, int x) {
+  if (p -> left == NULL) {
+    p -> left = makeNode(x);
+  } else {
+    printf("Left-child already exists\n");
+  }
+  return (p -> left);
 }
 
-struct node *rightChild(struct node *p, int x)
-{
-	if(p->right==NULL)
-	{
-        p->right=makeNode(x);
-	}
-	else
-	{
-		printf("Right-child already exists\n");
-	}
-	return (p->right);
+struct node * rightChild(struct node * p, int x) {
+  if (p -> right == NULL) {
+    p -> right = makeNode(x);
+  } else {
+    printf("Right-child already exists\n");
+  }
+  return (p -> right);
 }
 
 /* Pre-order traversal
@@ -78,13 +68,12 @@ void postOrder(struct node *t)
 }
 */
 
-void main()
-{
-	struct node *root, *A, *B, *C;
-	root=makeNode(1);
-	leftChild(root, 2);
-	rightChild(root, 3);
-	leftChild(root->right, 4);
-	rightChild(root->right, 5);
-	printf("Binary-Tree created");
+void main() {
+  struct node * root, * A, * B, * C;
+  root = makeNode(1);
+  leftChild(root, 2);
+  rightChild(root, 3);
+  leftChild(root -> right, 4);
+  rightChild(root -> right, 5);
+  printf("Binary-Tree created");
 }
